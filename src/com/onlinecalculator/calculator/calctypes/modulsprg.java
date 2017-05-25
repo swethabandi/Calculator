ea66e62b5192bd73d3808ad29685458b0785ecbe
iSTAR <iSTAR@192.168.100.17>
2017-05-25 09:04:57 +0530

    modulus program

---
+++ src/com/onlinecalculator/calculator/calctypes/modulsprg.java
@@ -0,0 +1,21 @@
+package com.onlinecalculator.calculator.calctypes;
+import java.util.*;
+public class modulsprg {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+		int a,b;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter a num..");
+		a=sc.nextInt();
+		System.out.println("enter b num..");
+		b=sc.nextInt();
+		System.out.println("modulus is..."+(a%b));
+		
+	
+		
+
+	}
+
+}
